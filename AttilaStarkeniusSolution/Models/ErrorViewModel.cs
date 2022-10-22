bd060be4e0bd55ab11f0c1baf1b0877eb130d4e1
Attila Starkenius <symphtom@hotmail.com>
2022-10-22 03:35:26 +0200

    Add project files.

---
+++ AttilaStarkeniusSolution/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AttilaStarkeniusSolution.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
