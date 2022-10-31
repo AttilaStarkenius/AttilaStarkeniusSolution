2bd8f45247eba7b423a1a4088caeaa18b6b351a4
Attila Starkenius <symphtom@hotmail.com>
2022-10-31 22:23:18 +0100

    31.10.2022. I do tutorial https://app.pluralsight.com/course-player?clipId=715b3831-58af-4507-bf32-7e0bb0673712

---
+++ HelloFromCSharp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello everybody");
+
+Console.WriteLine("Please enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name: ");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Hello " + firstName + " " + lastName);
+
+/*31.10.2022. I do tutorial https://app.pluralsight.com/course-player?clipId=715b3831-58af-4507-bf32-7e0bb0673712
+ and commit and push to Git Changes with the
+  message: "31.10.2022. I do tutorial https://app.pluralsight.com/course-player?clipId=715b3831-58af-4507-bf32-7e0bb0673712"*/
